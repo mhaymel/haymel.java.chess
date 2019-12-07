e25471f71ee7c0baa19c98d98b766eb0ed360517
Markus Heumel <markus.heumel@dynatrace.com>
2019-12-07 14:22:30 +0100

    project uciengine added

--- com.haymel.chess.uci/src/test/java/com/haymel/chess/uci/EngineTest.java
+++ com.haymel.chess.uci/src/test/java/com/haymel/chess/uci/EngineTest.java
@@ -7,27 +7,58 @@
  */
 package com.haymel.chess.uci;
 
+import static java.lang.String.format;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class EngineTest {
 
-	@Test
-	public void test() throws InterruptedException, UnsupportedEncodingException {
-		
-		ByteArrayInputStream in = new ByteArrayInputStream("uci\n".getBytes());
-		
-	    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-	    PrintStream out = new PrintStream(baos, true, "UTF-8");
+    private ByteArrayOutputStream baos;
+    private PrintStream out;
+    private Engine engine;
 
-		Engine worker = new Engine(in, out);
-		worker.start();
-		Thread.sleep(10);
-		worker.stop();
-	}
-
+    @Before
+    public void setup() throws UnsupportedEncodingException {
+	    baos = new ByteArrayOutputStream();
+	    out = new PrintStream(baos, true, "UTF-8");
+    }
+    
+    @Test
+    public void testUci() throws InterruptedException {
+    	ByteArrayInputStream in = new ByteArrayInputStream(format("uci%n").getBytes());
+    	engine = new FakeEngine(in, out);
+    	engine.start();
+    	Thread.sleep(10);
+    	String result = baos.toString(Charset.forName("UTF-8"));
+    	assertThat(result, is(String.format("id author Markus Heumel%nid name haymel%nuciok%n")));
+    }
+    
+    @Test
+    public void testIsReady() throws InterruptedException {
+    	ByteArrayInputStream in = new ByteArrayInputStream(format("isready%n").getBytes());
+    	engine = new FakeEngine(in, out);
+    	engine.start();
+    	Thread.sleep(10);
+    	String result = baos.toString(Charset.forName("UTF-8"));
+    	assertThat(result, is(String.format("readyok%n")));
+    }
+    
+    @Test
+    public void testPositionAndGo() throws InterruptedException {
+    	ByteArrayInputStream in = new ByteArrayInputStream(format("position startpos moves b1c3 b8c6%ngo wtime 100000 btime 100000 winc 1 binc 0%n").getBytes());
+    	engine = new FakeEngine(in, out);
+    	engine.start();
+    	Thread.sleep(1000);
+    	String result = baos.toString(Charset.forName("UTF-8"));
+    	assertThat(result, is(String.format("bestmove a1b1%n")));
+    }
 }
---
+++ com.haymel.chess.uci/src/test/java/com/haymel/chess/uci/FakeEngine.java
@@ -0,0 +1,73 @@
+
+/***************************************************
+ * (c) Markus Heumel
+ *
+ * @date:	07.12.2019
+ * @author: Markus.Heumel
+ *
+ */
+package com.haymel.chess.uci;
+
+import static com.haymel.util.Require.nonNull;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import com.haymel.chess.uci.moves.Moves;
+
+public class FakeEngine extends Engine {
+
+	private int plys;
+	
+	public FakeEngine(InputStream in, PrintStream out) {
+		super(nonNull(in, "in"), nonNull(out, "out"));
+	}
+	
+	@Override
+	public void uci() {
+		idauthor("Markus Heumel");
+		idname("haymel");
+		uciok();
+	}
+	
+	@Override
+	public void isReady() {
+		readyok();
+	}
+	
+	@Override
+	public void positionStart(Moves moves) {
+		plys = moves.value().size();
+	}
+	
+	@Override
+	public void go(int wtimeInSeconds, int btimeInSeconds, int wincInSeconds, int bincInSeconds) {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		switch(plys) {
+		case 0:
+			bestmove("b1c3");
+			break;
+		case 1:
+			bestmove("b8c6");
+			break;
+		case 2:
+			bestmove("a1b1");
+			break;
+		case 3:
+			bestmove("a8b8");
+			break;
+		case 4:
+			bestmove("b1a1");
+			break;
+		case 5:
+			bestmove("b8ba");
+			break;
+		}
+	}
+
+}
