10269c0d609a63f80228d0f1f8c6533d9f0c6595
Markus Heumel <markus.heumel@dynatrace.com>
2019-12-21 08:37:20 +0100

    PieceTest implemented

--- com.haymel.chess.engine/src/test/java/com/haymel/chess/engine/piece/PieceTest.java
+++ com.haymel.chess.engine/src/test/java/com/haymel/chess/engine/piece/PieceTest.java
@@ -7,10 +7,23 @@
  */
 package com.haymel.chess.engine.piece;
 
+import static com.haymel.chess.engine.board.Field.a1;
+import static com.haymel.chess.engine.board.Field.a8;
+import static com.haymel.chess.engine.board.Field.e1;
+import static com.haymel.chess.engine.board.Field.e8;
+import static com.haymel.chess.engine.board.Field.h1;
+import static com.haymel.chess.engine.board.Field.h8;
 import static com.haymel.chess.engine.board.Field.removed;
 import static com.haymel.chess.engine.piece.Piece.border;
 import static com.haymel.chess.engine.piece.Piece.free;
+import static com.haymel.chess.engine.piece.PieceType.BlackBishop;
+import static com.haymel.chess.engine.piece.PieceType.BlackKing;
+import static com.haymel.chess.engine.piece.PieceType.BlackKnight;
+import static com.haymel.chess.engine.piece.PieceType.BlackPawn;
+import static com.haymel.chess.engine.piece.PieceType.BlackQueen;
+import static com.haymel.chess.engine.piece.PieceType.BlackRook;
 import static com.haymel.chess.engine.piece.PieceType.WhiteBishop;
+import static com.haymel.chess.engine.piece.PieceType.WhiteKing;
 import static com.haymel.chess.engine.piece.PieceType.WhiteKnight;
 import static com.haymel.chess.engine.piece.PieceType.WhitePawn;
 import static com.haymel.chess.engine.piece.PieceType.WhiteQueen;
@@ -19,6 +32,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+
+import com.haymel.chess.engine.board.Field;
 
 public class PieceTest {
 
@@ -47,11 +62,9 @@ public class PieceTest {
 		assertThat(piece.white(), is(true));
 		assertThat(piece.blackKing(), is(false));
 		assertThat(piece.whiteKing(), is(false));
-
-		assertThat(piece.blackKing(), is(false));
-		assertThat(piece.whiteRook(), is(false));
 		assertThat(piece.blackRook(), is(false));
 		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
 	}
 
 	@Test
@@ -63,11 +76,19 @@ public class PieceTest {
 		assertThat(piece.white(), is(true));
 		assertThat(piece.blackKing(), is(false));
 		assertThat(piece.whiteKing(), is(false));
-		
-		assertThat(piece.blackKing(), is(false));
-		assertThat(piece.whiteRook(), is(false));
 		assertThat(piece.blackRook(), is(false));
 		assertThat(piece.whiteRook(), is(true));
+		assertThat(piece.moved(),is(true));
+
+		piece.field(a1);
+		piece.setMoved(false);
+		assertThat(piece.field(), is(a1));
+		assertThat(piece.moved(),is(false));
+		
+		piece.field(h1);
+		piece.setMoved(false);
+		assertThat(piece.field(), is(h1));
+		assertThat(piece.moved(),is(false));
 	}
 	
 	@Test
@@ -79,11 +100,9 @@ public class PieceTest {
 		assertThat(piece.white(), is(true));
 		assertThat(piece.blackKing(), is(false));
 		assertThat(piece.whiteKing(), is(false));
-
-		assertThat(piece.blackKing(), is(false));
-		assertThat(piece.whiteRook(), is(false));
 		assertThat(piece.blackRook(), is(false));
 		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
 	}
 
 	@Test
@@ -95,15 +114,13 @@ public class PieceTest {
 		assertThat(piece.white(), is(true));
 		assertThat(piece.blackKing(), is(false));
 		assertThat(piece.whiteKing(), is(false));
-
-		assertThat(piece.blackKing(), is(false));
-		assertThat(piece.whiteRook(), is(false));
 		assertThat(piece.blackRook(), is(false));
 		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
 	}
 
 	@Test
-	public void testWhiteWhiteQueen() {
+	public void testWhiteQueen() {
 		Piece piece = new Piece(WhiteQueen);
 
 		assertThat(piece.type(), is(WhiteQueen));
@@ -111,11 +128,127 @@ public class PieceTest {
 		assertThat(piece.white(), is(true));
 		assertThat(piece.blackKing(), is(false));
 		assertThat(piece.whiteKing(), is(false));
-
-		assertThat(piece.blackKing(), is(false));
-		assertThat(piece.whiteRook(), is(false));
 		assertThat(piece.blackRook(), is(false));
 		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+	}
+
+	@Test
+	public void testWhiteKing() {
+		Piece piece = new Piece(WhiteKing);
+
+		assertThat(piece.type(), is(WhiteKing));
+		assertThat(piece.black(), is(false));
+		assertThat(piece.white(), is(true));
+		assertThat(piece.blackKing(), is(false));
+		assertThat(piece.whiteKing(), is(true));
+		assertThat(piece.blackRook(), is(false));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+		
+		piece.field(e1);
+		piece.setMoved(false);
+		assertThat(piece.field(), is(e1));
+		assertThat(piece.moved(),is(false));
+	}
+	
+	@Test
+	public void testBlackPawn() {
+		Piece piece = new Piece(BlackPawn);
+
+		assertThat(piece.type(), is(BlackPawn));
+		assertThat(piece.black(), is(true));
+		assertThat(piece.white(), is(false));
+		assertThat(piece.blackKing(), is(false));
+		assertThat(piece.whiteKing(), is(false));
+		assertThat(piece.blackRook(), is(false));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+	}
+	
+	@Test
+	public void testBlackRook() {
+		Piece piece = new Piece(BlackRook);
+		
+		assertThat(piece.type(), is(BlackRook));
+		assertThat(piece.black(), is(true));
+		assertThat(piece.white(), is(false));
+		assertThat(piece.blackKing(), is(false));
+		assertThat(piece.whiteKing(), is(false));
+		assertThat(piece.blackRook(), is(true));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+
+		piece.field(a8);
+		piece.setMoved(false);
+		assertThat(piece.field(), is(a8));
+		assertThat(piece.moved(),is(false));
+		
+		piece.field(h8);
+		piece.setMoved(false);
+		assertThat(piece.field(), is(h8));
+		assertThat(piece.moved(),is(false));
+	}
+	
+	@Test
+	public void testBlackKnight() {
+		Piece piece = new Piece(BlackKnight);
+
+		assertThat(piece.type(), is(BlackKnight));
+		assertThat(piece.black(), is(true));
+		assertThat(piece.white(), is(false));
+		assertThat(piece.blackKing(), is(false));
+		assertThat(piece.whiteKing(), is(false));
+		assertThat(piece.blackRook(), is(false));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+	}
+
+	@Test
+	public void testBlackBishop() {
+		Piece piece = new Piece(BlackBishop);
+
+		assertThat(piece.type(), is(BlackBishop));
+		assertThat(piece.black(), is(true));
+		assertThat(piece.white(), is(false));
+		assertThat(piece.blackKing(), is(false));
+		assertThat(piece.whiteKing(), is(false));
+		assertThat(piece.blackRook(), is(false));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+	}
+
+	@Test
+	public void testWBlackQueen() {
+		Piece piece = new Piece(BlackQueen);
+
+		assertThat(piece.type(), is(BlackQueen));
+		assertThat(piece.black(), is(true));
+		assertThat(piece.white(), is(false));
+		assertThat(piece.blackKing(), is(false));
+		assertThat(piece.whiteKing(), is(false));
+		assertThat(piece.blackRook(), is(false));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+	}
+
+	@Test
+	public void testBlackKing() {
+		Piece piece = new Piece(BlackKing);
+
+		assertThat(piece.type(), is(BlackKing));
+		assertThat(piece.black(), is(true));
+		assertThat(piece.white(), is(false));
+		assertThat(piece.blackKing(), is(true));
+		assertThat(piece.whiteKing(), is(false));
+		assertThat(piece.blackRook(), is(false));
+		assertThat(piece.whiteRook(), is(false));
+		assertThat(piece.moved(),is(true));
+		
+		piece.field(e8);
+		piece.setMoved(false);
+		assertThat(piece.field(), is(e8));
+		assertThat(piece.moved(),is(false));
 	}
 
 }
